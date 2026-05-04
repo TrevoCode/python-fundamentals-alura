da01b7b4f0ce5e0fe9be0baee093ec9d5918b29c
Davi Rodrigues dos Santos Cariolano <davircariolano@gmail.com>
2026-05-03 22:39:32 -0300

    python

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+def finaliza_app():
+      os.system('cls')
+      print ('finalizado o app\n')
+
+print("1.Cadastrar restaurante")
+print("2.Lista restaurante")
+print("3.Ativar restaurante")
+print("4.sai\n")
+
+Opcao_escolha= int(input("Escolha uma opção"))
+
+
+if Opcao_escolha == 1 :
+     print('Cadastrar restaurante')
+elif Opcao_escolha == 2:
+      print('Lista restaurante')
+elif Opcao_escolha == 3:
+      print('Ativar restaurante')
+else :
+      print('Encerrado o programa')
+
+      finaliza_app()
---
+++ função_print.py
@@ -0,0 +1,12 @@
+print('Python na Escola de Programação da Alura')
+
+nome=input('Por favor escrever seu nome')
+idade=input('Por favor escrever seu idade')
+
+print('Meu nome é {nome} e tenho {idade} anos')
+
+print('A\nL\nu\nR\nA')
+
+pi = 3.14159
+
+print(f'pi:{pi:.2f}')
